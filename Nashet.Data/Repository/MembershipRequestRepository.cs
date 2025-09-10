80f2d44a1ff6cea2c67a4e18193c05b3705c4eee
نبراس صالح <221422576@student.kfu.edu.sa>
2025-09-10 11:23:12 +0300

    Tue

---
+++ Nashet.Data/Repository/MembershipRequestRepository.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Nashet.Data.Models;
+using Nashet.Data.Repository.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nashet.Data.Repository
+{
+    public class MembershipRequestRepository : BaseRepository<tblMembershipRequest>
+    {
+        public MembershipRequestRepository(NashetContext dbContext) : base(dbContext)
+        {
+        }
+        public virtual async Task<IList<tblMembershipRequest>> GetAllMembershipRequest()
+        {
+            return await dbSet.Where(MembershipRequest => MembershipRequest.IsDeleted == false).ToListAsync();
+        }
+        public virtual async Task<tblMembershipRequest> GetMembershipRequestById(int id)
+        {
+            return await dbSet.Where(MembershipRequest => MembershipRequest.IsDeleted == false && MembershipRequest.MRId == id)
+                .FirstOrDefaultAsync();
+        }
+        public virtual async Task<int> InsertMembershipRequest(tblMembershipRequest MembershipRequest)
+        {
+            try
+            {
+                await dbSet.AddAsync(MembershipRequest);
+                return 1;
+            }
+            catch
+            {
+                return 0;
+            }
+        }
+    }
+}
+//nn
---
+++ Nashet.Data/Repository/SystemLogsRepository.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Nashet.Data.Models;
+using Nashet.Data.Repository.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nashet.Data.Repository
+{
+    public class SystemLogsRepository : BaseRepository<tblSystemLogs>
+    {
+        public SystemLogsRepository(NashetContext dbContext) : base(dbContext)
+        {
+        }
+        public virtual async Task<int> InsertLog(tblSystemLogs Log)
+        {
+            try
+            {
+                await dbSet.AddAsync(Log);
+                return 1;
+            }
+            catch
+            {
+                return 0;
+            }
+        }
+
+
+    }
+}
