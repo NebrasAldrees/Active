c934ddcb38d70b50a88bc9deacb70b4772725e35
نبراس صالح <221422576@student.kfu.edu.sa>
2025-09-16 04:25:40 +0300

    15 Sep by N

--- Nashet/Areas/ActivitiesSupervisor/Controllers/HomeController.cs
+++ Nashet/Areas/ActivitiesSupervisor/Controllers/HomeController.cs
@@ -33,17 +33,12 @@ namespace Nashet.Areas.ActivitiesSupervisor.Controllers
             _ClubDomain = clubDomain;
             _ReportDomain = reportDomain;
         }
-<<<<<<< HEAD
-<<<<<<< HEAD
         
         
         public IActionResult ActivitiesSupervisorHome()
         {
             return View();
         }
-=======
-=======
->>>>>>> 5da93367c7711c4edcd5cf0ec7e041a8627bd7c6
         public IActionResult Index()
         {
             return View();
@@ -52,24 +47,12 @@ namespace Nashet.Areas.ActivitiesSupervisor.Controllers
         {
             return View();
         }
-        public IActionResult ActivitiesSupervisorHome()
-        {
-            return View();
-        }
-<<<<<<< HEAD
->>>>>>> 5da93367c7711c4edcd5cf0ec7e041a8627bd7c6
-=======
->>>>>>> 5da93367c7711c4edcd5cf0ec7e041a8627bd7c6
+        
         public IActionResult Clubs()
         {
             return View();
         }
         
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
->>>>>>> 5da93367c7711c4edcd5cf0ec7e041a8627bd7c6
         public IActionResult InsertClub()
         {
             return View();
@@ -78,87 +61,12 @@ namespace Nashet.Areas.ActivitiesSupervisor.Controllers
         {
             return View();
         }
-        public IActionResult InsertAnnouncement()
-        {
-            return View();
-        }
-<<<<<<< HEAD
->>>>>>> 5da93367c7711c4edcd5cf0ec7e041a8627bd7c6
-=======
->>>>>>> 5da93367c7711c4edcd5cf0ec7e041a8627bd7c6
-        public IActionResult Requests()
-        {
-            return View();
-        }
+        
         
         public async Task<IActionResult> ViewUsers()
         {
             return View(await _UserDomain.GetUserByIdAsync(UserId));
         }
-<<<<<<< HEAD
-<<<<<<< HEAD
-        
-=======
-        [HttpPost]
-        [ValidateAntiForgeryToken]
->>>>>>> 5da93367c7711c4edcd5cf0ec7e041a8627bd7c6
-=======
-        [HttpPost]
-        [ValidateAntiForgeryToken]
->>>>>>> 5da93367c7711c4edcd5cf0ec7e041a8627bd7c6
-
-        public IActionResult InsertUser()
-        {
-            return View();
-        }
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        
-        public async Task<IActionResult> ViewTeams()
-        {
-            return View(await _TeamDomain.GetTeam());
-        }
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-        public IActionResult InsertAnnouncement()
-        {
-            return View();
-        }
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-=======
-=======
->>>>>>> 5da93367c7711c4edcd5cf0ec7e041a8627bd7c6
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-
-        public async Task<IActionResult> InsertActivity(tblActivity Activity)
-        {
-            try
-            {
-                int check = await _ActivityDomain.InsertActivity(Activity);
-                if (check == 1)
-                    ViewBag.Successful = "Successful";
-                else
-                    ViewBag.Failed = "Failed";
-            }
-            catch { }
-            return View(Activity);
-        }
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public async Task<IActionResult> ViewActivities()
-        {
-            return View(await _ActivityDomain.GetActivity());
-        }
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-
-
-<<<<<<< HEAD
->>>>>>> 5da93367c7711c4edcd5cf0ec7e041a8627bd7c6
-=======
->>>>>>> 5da93367c7711c4edcd5cf0ec7e041a8627bd7c6
         public async Task<IActionResult> InsertAnnouncement(tblAnnouncement Announcement)
         {
             try
@@ -177,26 +85,10 @@ namespace Nashet.Areas.ActivitiesSupervisor.Controllers
         {
             return View(await _AnnouncementDomain.GetAnnouncement());
         }
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-        public IActionResult InsertClub()
-        {
-            return View();
-        }
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-=======
-=======
->>>>>>> 5da93367c7711c4edcd5cf0ec7e041a8627bd7c6
-        [HttpPost]
-        [ValidateAntiForgeryToken]
 
 
-<<<<<<< HEAD
->>>>>>> 5da93367c7711c4edcd5cf0ec7e041a8627bd7c6
-=======
->>>>>>> 5da93367c7711c4edcd5cf0ec7e041a8627bd7c6
+
+
         public async Task<IActionResult> InsertClub(tblClub Club)
         {
             try
@@ -210,44 +102,13 @@ namespace Nashet.Areas.ActivitiesSupervisor.Controllers
             catch { }
             return View(Club);
         }
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
->>>>>>> 5da93367c7711c4edcd5cf0ec7e041a8627bd7c6
-=======
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
->>>>>>> 5da93367c7711c4edcd5cf0ec7e041a8627bd7c6
+
         public async Task<IActionResult> ViewClubs()
         {
             return View(await _ClubDomain.GetClub());
         }
-<<<<<<< HEAD
-<<<<<<< HEAD
-        
-=======
-        [HttpPost]
-        [ValidateAntiForgeryToken]
->>>>>>> 5da93367c7711c4edcd5cf0ec7e041a8627bd7c6
-=======
-        [HttpPost]
-        [ValidateAntiForgeryToken]
->>>>>>> 5da93367c7711c4edcd5cf0ec7e041a8627bd7c6
-        //public async Task<IActionResult> Requests()
-        //{
-        //    return View(await _ActivityRequestDomain.GetActivityRequest());
-        //}
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-<<<<<<< HEAD
-<<<<<<< HEAD
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-=======
-
->>>>>>> 5da93367c7711c4edcd5cf0ec7e041a8627bd7c6
-=======
-
->>>>>>> 5da93367c7711c4edcd5cf0ec7e041a8627bd7c6
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
