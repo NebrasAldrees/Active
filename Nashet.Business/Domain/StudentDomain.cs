0f371e499f3ed0e1c5b5f9a92916284b97256f71
MuntahaAb <muntaha-12@outlook.com>
2025-09-18 11:03:37 +0300

    by muntaha

--- Nashet.Business/Domain/StudentDomain.cs
+++ Nashet.Business/Domain/StudentDomain.cs
@@ -16,54 +16,51 @@ namespace Nashet.Business.Domain
     {
         private readonly StudentRepository _StudentRepository = Repository;
         
-        public async Task<IList<tblStudent>> GetStudent()
+        
+
+        public async Task<IList<StudentViewModel>> GetStudent()
         {
-            return await _StudentRepository.GetAllStudents();
+            return _StudentRepository.GetAllStudents().Result.Select(S => new StudentViewModel
+            {
+                StudentId = S.StudentId,
+                AcademicId = S.AcademicId,
+                StudentNameAr = S.StudentNameAr,
+                StudentNameEn = S.StudentNameEn,
+                StudentEmail = S.StudentEmail,
+                StudentPhone = S.StudentPhone,
+                StudentSkills = S.StudentSkills,
+                SiteId = S.SiteId,
+                Site = S.Site
+
+            }).ToList();
         }
 
-        //public async Task<IList<StudentViewModel>> GetStudent()
-        //{
-        //    return _StudentRepository.GetAllStudents).Result.Select(t => new StudentViewModel
-        //    {
-        //        StudentId = S.StudentId,
-                //            AcademicId = S.AcademicId,
-                //            StudentNameAr = S.StudentNameAr,
-                //            StudentNameEn  = S.StudentNameEn,
-                //            StudentEmail  = S.StudentEmail,
-                //            StudentPhone = S.StudentPhone,
-                //            StudentSkills = S.StudentSkills,
-                //            SiteId = S.SiteId,
-                //            Site = S.Site
-
-        //    }).ToList();
-        //}
-
-        //public async Task<int> InsertStudent(StudentViewModel viewModel)
-        //{
-        //    try
-        //    {
-        //        tblStudent Student = new tblStudent
-        //        {
-        //            StudentId= viewModel.StudentId,
-        //            AcademicId = viewModel.AcademicId,
-        //            StudentNameAr = viewModel.StudentNameAr,
-        //            StudentNameEn  = viewModel.StudentNameEn,
-        //            StudentEmail  = viewModel.StudentEmail,
-        //            StudentPhone = viewModel.StudentPhone,
-        //            StudentSkills = viewModel.StudentSkills,
-        //            SiteId = viewModel.SiteId,
-        //            Site = viewModel.Site
-        //        };
-        //        int check = await _StudentRepository.InsertStudent(Student);
-        //        if (check == 0)
-        //            return 0;
-        //        else
-        //            return 1;
-        //    }
-        //    catch
-        //    {
-        //        return 0;
-        //    }
+        public async Task<int> InsertStudent(StudentViewModel viewModel)
+        {
+            try
+            {
+                tblStudent Student = new tblStudent
+                {
+                    StudentId = viewModel.StudentId,
+                    AcademicId = viewModel.AcademicId,
+                    StudentNameAr = viewModel.StudentNameAr,
+                    StudentNameEn = viewModel.StudentNameEn,
+                    StudentEmail = viewModel.StudentEmail,
+                    StudentPhone = viewModel.StudentPhone,
+                    StudentSkills = viewModel.StudentSkills,
+                    SiteId = viewModel.SiteId,
+                    Site = viewModel.Site
+                };
+                int check = await _StudentRepository.InsertStudent(Student);
+                if (check == 0)
+                    return 0;
+                else
+                    return 1;
+            }
+            catch
+            {
+                return 0;
+            }
 
         public async Task<int> InsertStudent(StudentViewModel viewModel)
         {
