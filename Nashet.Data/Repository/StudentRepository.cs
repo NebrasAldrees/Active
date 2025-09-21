802986d6ef06c2da04d7d95875ab89daeeed1f19
huavilf . <huavilf@gmail.com>
2025-09-21 11:23:11 +0300

    hh

--- Nashet.Data/Repository/StudentRepository.cs
+++ Nashet.Data/Repository/StudentRepository.cs
@@ -14,14 +14,9 @@ namespace Nashet.Data.Repository
         public StudentRepository(NashetContext dbContext) : base(dbContext)
         {
         }
-        public virtual async Task<IList<tblStudent>> GetAllStudents(int id)
-        {
-            return await dbSet.Where(Student => Student.IsDeleted == false && Student.SiteId == id).ToListAsync(); 
-        }
-
         public virtual async Task<IList<tblStudent>> GetAllStudents()
         {
-            throw new NotImplementedException();
+            return await dbSet.Where(Student => Student.IsDeleted == false ).ToListAsync(); 
         }
 
         public virtual async Task<tblStudent> GetStudentByIdAsync(int id)
