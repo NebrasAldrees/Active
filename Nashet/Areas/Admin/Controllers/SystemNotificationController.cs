e424cd1e3ad124c16e128bfac92ad8a33a782a53
nalm2rri <Nalma2rri@gmail.com>
2025-09-22 11:13:28 +0300

    by n

---
+++ Nashet/Areas/Admin/Controllers/SystemNotificationController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Nashet.Business.Domain;
+using Nashet.Business.ViewModels;
+
+namespace Nashet.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class SystemNotificationController : Controller
+    {
+
+        private readonly SystemNotificationDomain _SystemNotificationDomain;
+        public SystemNotificationController(SystemNotificationDomain SystemNotificationDomain)
+        {
+            _SystemNotificationDomain = SystemNotificationDomain;
+        }
+        public async Task<IActionResult> ViewNotification()
+        {
+            return View(await _SystemNotificationDomain.GetAllNotifications());
+        }
+        public async Task<IActionResult> InsertNotification()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> InsertNotification(StudentViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    int check = await _SystemNotificationDomain.InsertNotification(viewModel);
+                    if (check == 1)
+                        ViewData["Successful"] = "Successful";
+                    else
+                        ViewData["Failed"] = "Failed";
+                }
+                catch
+                {
+                    ViewData["Failed"] = "Failed";
+                }
+            }
+            return View(viewModel);
+        }
+
+
+
+    }
+}
