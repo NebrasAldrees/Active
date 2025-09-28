1d42204e7be5e6c48fe8590515f2be3c57583ba2
MuntahaAb <muntaha-12@outlook.com>
2025-09-28 10:27:22 +0300

    u

--- Nashet/Areas/ClubSupervisor/Controllers/MembershipController.cs
+++ Nashet/Areas/ClubSupervisor/Controllers/MembershipController.cs
@@ -4,50 +4,50 @@ using Nashet.Business.ViewModels;
 
 namespace Nashet.Areas.ClubSupervisor.Controllers
 {
-    
-        [Area("ClubSupervisor")]
-        public class MembershipController : Controller
-        {
-            private readonly MembershipDomain _MembershipDomain;
-            public MembershipController(MembershipDomain MembershipDomain)
-            {
-                _MembershipDomain = MembershipDomain;
-            }
-            public async Task<IActionResult> ViewMember()
-            {
-                return View(await _MembershipDomain.GetMembership());
-            }
-            public async Task<IActionResult> InsertMember()
-            {
-                return View();
-            }
-            [HttpPost]
-            [ValidateAntiForgeryToken]
-            public async Task<IActionResult> InsertMember(MembershipViewModel viewModel)
-            {
-                if (ModelState.IsValid)
-                {
-                    try
-                    {
-                        int check = await _MembershipDomain.InsertMembership(viewModel);
-                        if (check == 1)
-                            ViewData["Successful"] = "Successful";
-                        else
-                            ViewData["Failed"] = "Failed";
-                    }
-                    catch
-                    {
-                        ViewData["Failed"] = "Failed";
-                    }
-                }
-                return View(viewModel);
-            }
 
-            public IActionResult Index()
+    [Area("ClubSupervisor")]
+    public class MembershipController : Controller
+    {
+        private readonly MembershipDomain _MembershipDomain;
+        public MembershipController(MembershipDomain MembershipDomain)
+        {
+            _MembershipDomain = MembershipDomain;
+        }
+        public async Task<IActionResult> ViewMember()
+        {
+            return View(await _MembershipDomain.GetMembership());
+        }
+        public async Task<IActionResult> InsertMember()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> InsertMember(MembershipViewModel viewModel)
+        {
+            if (ModelState.IsValid)
             {
-                return View();
+                try
+                {
+                    int check = await _MembershipDomain.InsertMembership(viewModel);
+                    if (check == 1)
+                        ViewData["Successful"] = "Successful";
+                    else
+                        ViewData["Failed"] = "Failed";
+                }
+                catch
+                {
+                    ViewData["Failed"] = "Failed";
+                }
             }
+            return View(viewModel);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
         }
     }
+}
 
 
