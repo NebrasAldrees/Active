813c07ffe92332e397cd6f2f7938697ea99ca0df
Nebras <nebras.s423@gmail.com>
2025-10-01 11:19:44 +0300

    By N

--- Nashet.Data/Migrations/20250928083830_AddedByNebras.cs
+++ Nashet.Data/Migrations/20250928083830_AddedByNebras.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace Nashet.Data.Migrations
 {
     /// <inheritdoc />
-    public partial class AddMuntaha : Migration
+    public partial class AddedByNebras : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
